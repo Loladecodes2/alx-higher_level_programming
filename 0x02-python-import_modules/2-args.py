73a93efbc0c6a3d102674047f9ce1b138aebf2fd
Loladecodes <Ololade220@gmail.com>
2022-09-22 15:48:39 -0700

    task-2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
-#!/usr,bin/python3
-
+#!/usr/bin/python3
 if __name__ == '__main__':
     import sys
 
-    num = len(sys.argv)
-    if num == 0:
-        print("{} argument.".format(num - 1))
-    elif num == 1:
-        print("{} argument:".format(num - 1))
+    num = len(sys.arv) - 1
+    if num == 1:
+        print("1 argument:")
+    elif num == 0:
+        print("0 argument.")
     else:
-        print("{} arguments:".format(num - 1))
-    for n in range(1, num):
-        print("{}: {}".format(n, sys.argv[n]))
+        print("{} argument:".format(num))
+    for n in range(num):
+        print("{} : {}".format(n + 1, sys.argv[n + 1]))
