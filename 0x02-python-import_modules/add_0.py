939350eb3c52d119c646500446dca89582a00b41
Loladecodes <Ololade220@gmail.com>
2022-09-22 16:01:57 -0700

    all tasks

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def add(a, b):
-    """ add function
-    argument: a , b
+    """My addition function
 
-    return : the value a + b
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
     """
     return (a + b)
