5f40e58ceead3b17205bc6b1ab674a937e520643
Ololade Akinloye <ololade220@gmail.com>
2023-01-17 16:28:11 -0800

    network

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,11 +5,12 @@
 -   found in the header of the response
 """
 import sys
+
 import urllib.request
 
 if __name__ == "__main__":
-     url = sys.argv[1]
+    url = sys.argv[1]
 
-     request = urllib.request.Request(url)
-     with urllib.request.urlopen(request) as response:
-         print(dict(response.headers).get("X-Request-Id"))
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
